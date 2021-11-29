778b3594e2a595193c23fa07b94c000dd9541f87
Maciej Szymanski <10569@student.puzim.edu.pl>
2021-11-29 12:58:26 +0100

    fixed code

--- Program.cs
+++ Program.cs
@@ -8,10 +8,10 @@ namespace mprog_lab_01
     {
         static void Main(string[] args)
         {
-            int x = 0
+            int x = 0;
             while(x<5){
                 Console.WriteLine("Hello World!");
-                x++
+                x++;
                        }
         }
     }
